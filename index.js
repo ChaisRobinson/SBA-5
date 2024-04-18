9f07616c8ecd9723023ff68fd96f283c391347bc
Chazz Robinson <chais5robinson@gmail.com>
2024-04-18 09:40:02 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Start Express server
+const express = require("express");
+const app = express();
+const port = 3000;
+
+//Midelware
+
+// Routes
+
+// Home Route
+
+// Error Route
+
+// Error Handler middleware
+
+// Listen on port 3000
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
