42d5aa2f9fa5f8be9ab8dff32f70701fb947ba09
ChaisRobinson <chais5robinson@gmail.com>
2024-04-19 14:35:05 -0400

    added GET id index for mtg

--- Routes_Veiws/dataThree.js
+++ Routes_Veiws/dataThree.js
@@ -7,6 +7,16 @@ router.get("/", (req, res) => {
   res.json(mtgBoosterPacks);
 });
 
+// GET /mtgBoosterPacks/:id
+router.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const pack = mtgBoosterPacks.find((pack) => pack.id === id);
+  if (pack) {
+    res.json(pack);
+  } else {
+    res.status(404).json({ error: "Pack not found" });
+  }
+})
 
 // Export router
 module.exports = router;
