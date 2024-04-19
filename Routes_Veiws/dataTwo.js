fd47f569869dfd93654e911bbc8ed7019d2fce6c
ChaisRobinson <chais5robinson@gmail.com>
2024-04-19 10:42:19 -0400

    fixed yu-gi-oh template engine

--- Routes_Veiws/dataTwo.js
+++ Routes_Veiws/dataTwo.js
@@ -8,26 +8,6 @@ router.use(express.static("./styles"));
 // Require the filesystem module
 const fs = require("fs");
 
-// Define the template engine
-router.engine("yu_gi_oh", (filePath, options, callback) => {
-  fs.readFile(filePath, (err, content) => {
-    if (err) return callback(err);
-
-    // Here, we take the content of the template file,
-    // convert it to a string, and replace sections of
-    // it with the values being passed to the engine.
-    const rendered = content
-      .toString()
-      .replace("#title#", `${options.title}`)
-      .replace("#content#", `${options.renderContent()}`);
-
-    return callback(null, rendered);
-  });
-});
-
-router.set("views", "./views"); // Specify the views directory
-router.set("view engine", "yu_gi_oh"); // Register the template engine
-
 // GET /yugiohBoosterPacks
 router.get("/", (req, res) => {
   const options = {
@@ -47,5 +27,7 @@ router.get("/", (req, res) => {
   };
   res.render("index", options);
 });
+
+
 
 module.exports = router;
--- index.js
+++ index.js
@@ -5,7 +5,6 @@ process.env.NODE_ENV = "development";
 const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 
-
 // Import data
 const pokemonBoosterPacks = require("./Data/dataOne");
 const yugiohBoosterPacks = require("./Data/dataTwo");
@@ -45,6 +44,30 @@ app.use((req, res, next) => {
 app.use("/pokemonBoosterPacks", pokemonRoutes);
 app.use("/yugiohBoosterPacks", yugiohRoutes);
 app.use("/mtgBoosterPacks", mtgRoutes);
+
+// Yu-Gi-Oh! Template Engine
+// Require the filesystem module
+const fs = require("fs");
+
+// Define the template engine
+app.engine("yu_gi_oh", (filePath, options, callback) => {
+  fs.readFile(filePath, (err, content) => {
+    if (err) return callback(err);
+
+    // Here, we take the content of the template file,
+    // convert it to a string, and replace sections of
+    // it with the values being passed to the engine.
+    const rendered = content
+      .toString()
+      .replace("#title#", `${options.title}`)
+      .replace("#content#", `${options.renderContent()}`);
+
+    return callback(null, rendered);
+  });
+});
+
+app.set("views", "./views"); // Specify the views directory
+app.set("view engine", "yu_gi_oh"); // Register the template engine
 
 // Home Route
 app.get("/", (req, res) => {
